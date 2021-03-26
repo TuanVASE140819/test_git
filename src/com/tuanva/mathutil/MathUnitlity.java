2bd8498510c739dc5c2c08dc5e1f1f1995186ab5
doit-now <tuanvase140819@gmail.com>
2021-03-26 15:07:51 +0700

    Update core to add the recursiation. GREEN 4EVER AGAIN

--- src/com/tuanva/mathutil/MathUnitlity.java
+++ src/com/tuanva/mathutil/MathUnitlity.java
@@ -10,30 +10,41 @@ package com.tuanva.mathutil;
  * @author TuanVA
  */
 public class MathUnitlity {
+
     // ta se tao cac ham trong class nay  fake y chang class math cua JDK
     // cac ham se xai chung o nhieu no, lam tien ich dung chung
     // pham cai gi dung chung tho ta suy nghi den static
-    public  static final double  PI = 3.1415;// Y chang Math.PI
-                                             // lat hoi MathUtility.PI
+    public static final double PI = 3.1415;// Y chang Math.PI
+    // lat hoi MathUtility.PI
     //ham tinh n!= 1.2.3.4.5.6..n
     //quy uoc 0!=1!=2!
     // ko co giai thua am
     // vi giai thua tang cuc nhanh ve gia tri: 21 g kieu lonh
     //ta khong chue n dau vao cac ham tu: 0..20
     //kieu long 10^18, 18 con so 0 toi da:
-    
-    public  static long getFactorial(int n){
-        if (n<0 || n>20) 
+
+//    public  static long getFactorial(int n){
+//        if (n<0 || n>20) 
+//            throw new IllegalArgumentException("Invalid argument."
+//                                              + " n must be between 0.. 20");
+//            if (n==0 || n==1) 
+//                return 0; //0! =1! =1 tin luon cho nhanh
+//            //den doan nay sure n tu 2..20
+//            long result =1;
+//            for (int i = 2; i <=n; i++) {
+//            result =result*i;
+//        }
+//        return result; 
+//    }
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument."
-                                              + " n must be between 0.. 20");
-            if (n==0 || n==1) 
-                return 0; //0! =1! =1 tin luon cho nhanh
-            //den doan nay sure n tu 2..20
-            long result =1;
-            for (int i = 2; i <=n; i++) {
-            result =result*i;
+                    + " n must be between 0.. 20");
         }
-        return result; 
+        if (n == 0 || n == 1) 
+            return 1; //0! =1! =1 tin luon cho nhanh
+                  // minh thua biet n!=n*(n-1)!
+        return n * getFactorial(n - 1);
     }
-    
+
 }
